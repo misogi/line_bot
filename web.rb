d899f6d7b892060251dbc99d0e65a46a65c1d455
misogi <dev@misogi.info>
2016-04-09 15:14:34 +0900

    use rest-client

--- web.rb
+++ web.rb
@@ -1,18 +1,15 @@
 require 'sinatra'
 require 'json'
-require 'httpclient'
 require 'pp'
+require 'rest-client'
 
 line_headers = {
-  'Connection' => 'close',
   'Content-Type' => 'application/json; charset=UTF-8',
   'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
   'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
   'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"]
 }
 line_url = 'https://trialbot-api.line.me/v1/events'
-channel = 1383378250
-type = '138311609000106303'
 
 get '/' do
   'OK'
@@ -24,7 +21,7 @@ post '/callback' do
   results = params['result']
   result = results[0]
   to = result['from']
-  h = HTTPClient.new(ENV["FIXIE_URL"])
+  RestClient.proxy = ENV["FIXIE_URL"]
   line_content = {
     to: [to],
     toChannel: 1383378250,
@@ -37,12 +34,6 @@ post '/callback' do
   }
 
   begin
-    h.post_content(line_url, body: line_content.to_json, header: line_headers)
-  rescue HTTPClient::BadResponseError => e
-    puts e.res.body
-  rescue HTTPClient::KeepAliveDisconnected => e
-    puts 'keep alive disconnected'
-    pp e.sess.get_body
-    pp e.sess
+    RestClient.post(line_url, line_content.to_json, line_headers)
   end
 end
